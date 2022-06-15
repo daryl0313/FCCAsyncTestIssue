5c2fc4e06f384fb8acb0363155932d2691bc36b5
Derek Batenhorst <derek.batenhorst@buildertrend.com>
2022-06-15 11:01:57 -0500

    Add project files.

---
+++ FrameworkLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FrameworkLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BuilerTrend")]
+[assembly: AssemblyProduct("FrameworkLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright © BuilerTrend 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0d53c08-840e-4f86-a9b9-e1af6e1ccc7c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FrameworkLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace FrameworkLibrary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldTest_NonAsyncMethod()
+        {
+            var fccClass = new FccClass();
+            var actual = fccClass.NonAsyncMethod();
+
+            Assert.IsNotNull(actual);
+        }
+        [TestMethod]
+        public async Task ShouldTest_AsyncMethod()
+        {
+            var fccClass = new FccClass();
+            var actual = await fccClass.AsyncMethod();
+
+            Assert.IsNotNull(actual);
+        }
+    }
+}
