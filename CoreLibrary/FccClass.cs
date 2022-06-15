a060d7ce271f0e1b7d73ffe3c6a3520605d3608d
Derek Batenhorst <derek.batenhorst@buildertrend.com>
2022-06-15 11:15:08 -0500

    Added dotnet 6 examples

---
+++ CoreLibrary/FccClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkLibrary
+{
+    public class FccClass
+    {
+        public string NonAsyncMethod()
+        {
+            return "result";
+        }
+        public async Task<string> AsyncMethod()
+        {
+            return "result";
+        }
+    }
+}
